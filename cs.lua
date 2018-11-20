66529eb074b46a7826a0d93e57a883aa18172e82
aniket sharma <aniket965.as@gmail.com>
2018-11-20 23:47:38 +0530

    Create cs.lua

---
+++ cs.lua
@@ -0,0 +1,146 @@
+function sysCall_init()
+    -- This child script contains computing path and publishing it into the path_planning.py for waypoint navigation.
+    
+
+    -- Declaring required handles
+    drone_handle = sim.getObjectHandle('eDroneBase')
+    collection_handles= sim.getCollectionHandle('Obstacles')
+
+    -- Assigning obstacles handles
+    no_of_obstacles = 6
+    obstacles_handles = {}
+    for i=1,no_of_obstacles do
+        table.insert(obstacles_handles,sim.getObjectHandle('obstacle_'..tostring(i)))
+    end
+
+    -----------Add other required handles here----------------
+    no_of_goals = 2
+    goals_handles = {}
+    for i=1,no_of_goals do
+        table.insert(goals_handles,sim.getObjectHandle('goal_'..tostring(i)))
+    end
+    target_handle = sim.getObjectHandle('target')
+    --Hint : Goal handles and other required handles
+    ----------------------------------------------------------
+
+
+    ------------Add the path planning task initial details------------------
+
+
+
+
+    --Hint : Creating task, statespace creation, algorithm setting and setting collision pairs
+    -- Carefull about the bounds and the collision pairs you set.
+    --------------------------------------------------------------------------
+
+
+    --------------------Add your publisher and subscriber nodes here ---------------------
+
+    path_pub=simROS.advertise('/vrep/waypoints', 'geometry_msgs/PoseArray')    -- Publish the computed path under the topic /vrep/waypoints
+    
+
+
+
+
+    -- Hint : You will require to subscribe to a topic published by path_planning.py indicating the need of new path once all waypoints are covered. THIS IS IMPORTANT
+    ---------------------------------------------------------------------------------------
+
+
+    -- scale_factor = {} -- Add the scale_factor you computed learned from the tutorial of whycon transformation
+    -- no_of_path_points_required = -- Add no of path points you want from one point to another
+
+end
+
+
+-- This function can be used to visualize the path you compute. This function takes path points as the argument...
+-- GO through the code segment for better understanding
+function visualizePath( path )
+    if not _lineContainer then
+        _lineContainer=sim.addDrawingObject(sim.drawing_lines,1,0,-1,99999,{0.2,0.2,1})
+    end
+    sim.addDrawingObjectItem(_lineContainer,nil)
+    if path then
+        local pc=#path/7
+        for i=1,pc-1,1 do
+            lineDat={path[(i-1)*7+1],path[(i-1)*7+2],path[(i-1)*7+3],path[i*7+1],path[i*7+2],path[i*7+3]}
+            sim.addDrawingObjectItem(_lineContainer,lineDat)
+        end
+    end
+end
+
+
+-- This function is used to send the Path computed in the real_world to whycon_world after transformation
+-- Message is to be sent in geometry_msgs/PoseArray as WhyCon node subscribes to that message
+function packdata(path)
+
+    local sender = {header = {}, poses = {}}
+    
+    sender['header']={seq=123, stamp=simROS.getTime(), frame_id="drone"}
+    sender['poses'] = {}
+
+    for i=1,((no_of_path_points_required-1)*7)+1,7 do
+        a = {x = 0, y = 0, w = 0, z = 0}
+        b = {x = 0, y = 0, z = 0}
+        pose = {position = b, orientation = a, }
+
+        -------------------Add x, y and z value after converting real_world to whycon_world using the computed scale_factor--------------------------------
+
+        -- pose.position.x = 
+        -- pose.position.y = 
+        -- pose.position.z = 
+        sender.poses[math.floor(i/7) + 1] = pose
+
+
+        --------------------------------------------------------------------------------------------------------------------
+    end
+    -- Debug if the path computed are correct. Display the computed path and see if the path points moves from drone to the target point
+    return sender
+end
+
+
+--- This function is used to compute and publish the path to path_planninglpy
+function compute_and_send_path(task)
+    local r
+    local path
+
+    -- r,path=simOMPL.compute(,,,) -- Provide the correct arguments here.. Make sure you check the no of path points it actually computes
+
+    if(r == true) then
+        visualizePath(path)
+        message = packdata(path)  
+        simROS.publish(path_pub,message)
+        -- Provide slots for debug to cross check the message or path points you recieve after computing
+    end
+    return r
+end
+
+
+function sysCall_actuation()
+    
+    ---- Add your code to set start and goal state after getting present poses of the drone and the new goal when path_planning.py request you a path
+    ---------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+    ----------------------------------------------------------------------------------------------------------------
+    --Hint : Set orientation of the goal as same as that of the start pose as you don't want the drone to rotate on its yaw
+
+end
+
+
+function sysCall_sensing()
+
+end
+
+
+
+function sysCall_cleanup()
+
+end
+
