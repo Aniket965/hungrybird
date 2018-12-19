9a02a738eeebba49a60dd18ab6e485d700e58999
aniket sharma <aniket965.as@gmail.com>
2018-12-19 22:42:38 +0530

    Update t2.lua

--- t2.lua
+++ t2.lua
@@ -1,12 +1,14 @@
 -- This script is used for realtime emulation of the environment in V-REP
-
+-- this scipts uses one aruco and one whycon marker at time to emulate arena in order
+-- cashew tree,sal tree, nonfood,tree
+-- press a for setting orientation, s for position, k to set next tree
 function sysCall_init()
 
     -- Add required handles here
 
     position_handles = {sim.getObjectHandle('Position_hoop3'),sim.getObjectHandle('Position_hoop1'),sim.getObjectHandle('obstacle_1')}
     orientation_handles = {sim.getObjectHandle('Orientation_hoop3'),sim.getObjectHandle('Orientation_hoop1')}
-
+    zvals = {0.5,1.1229,1.3345}
     is_current_orientation_set = false
     is_current_position_set = false
     
@@ -34,11 +36,13 @@ end
 function aruco_callback(msg)
     -- Get the orientation(quaternion) of the ArUco marker and set the orientation of the hoop using Orientation_hoop dummy
     -- Hint : Go through the regular API - sim.setObjectQuaternion
-    -- id 2 is for cashew tree
+     
     if is_current_orientation_set == false then
         pose = msg.markers[1].pose.pose.orientation
-        sim.setObjectQuaternion(orientation_handles[1],-1,{pose.x,pose.y,-pose.z,-pose.w})
+        sim.setObjectQuaternion(orientation_handles[1],-1,{-pose.x,pose.y,-pose.z,pose.w})
 	end
+    print(sim.getObjectQuaternion(sim.getObjectHandle('Orientation_hoop1'),-1))
+    print({pose.x,pose.y,pose.z,-pose.w})
 
 
 end
@@ -48,30 +52,35 @@ function whycon_callback(msg)
     -- Hint : Go through the regular API - sim.setObjectPosition
     if is_current_position_set == false then
         pos = msg.poses[1].position
-        sim.setObjectPosition(position_handles[1],-1,{pos.x/4,-pos.y/4,0.5}) 
+        sim.setObjectPosition(position_handles[1],-1,{-pos.x/4,pos.y/4,zvals[1]}) 
     end
 end
 
 function key_callback(msg)
     -- Read key input to set or unset position and orientation of food and non-food trees
     if msg.data == 70 then
-        -- set orientation of current tree
+        -- set orientation of current tree (a)
         is_current_orientation_set = true
         print('current orientation set')
     elseif msg.data == 60 then
-        -- set position of current tree
+        -- set position of current tree (s)
         is_current_position_set = true
         print('current position set')
     elseif msg.data == 0 then
-        -- unset current tree orientation 
+        -- unset current tree orientation(d) 
         is_current_orientation_set = false
         print('unset current orientation')
     elseif msg.data == 10 then
-        -- unset current tree  position 
+        -- unset current tree  position (i) 
         is_current_position_set = false
         print('unset current position')
     elseif msg.data == 20 then
-        -- move to next tree
+        -- move to next tree(k)
+        table.remove(position_handles,1)
+        table.remove(orientation_handles,1)
+        table.remove(zvals,1)
+        is_current_position_set = false
+        is_current_orientation_set = false
     else
         print('Not valid command for this script')
     end
